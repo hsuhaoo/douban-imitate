5b9b229f8c52ff622b0bea9db2a04613db4cc2bb
hsuhaoo <fk3500@gmail.com>
2020-12-25 21:55:18 +0800

    add more pages

---
+++ douban-vue/src/components/subject.vue
@@ -0,0 +1,186 @@
+<template>
+    <div id="wrapper">
+        <h1>
+            <span property="v:itemreviewed">歌利亚</span>
+            <div class="clear"></div>
+        </h1>
+        <div id="content">
+            <div class="grid-16-8 clearfix">
+                <div class="article">
+                    <div class="indent">
+                        <div class="subjectwrap clearfix">
+                            <div class="subject clearfix">
+                                <div id="mainpic" class="">
+                                    <a class="nbg" 
+                                        title="歌利亚">
+                                        <img src="https://img1.doubanio.com/view/subject/s/public/s33766399.jpg"
+                                            title="点击看大图" alt="歌利亚" rel="v:photo">
+                                    </a>
+                                </div>
+                                <div id="info" class="">
+                                    <span>
+                                        <span class="pl"> 作者</span>:
+                                        <a class=""
+                                            href="/search/%E6%B1%A4%E5%A7%86%C2%B7%E9%AB%98%E5%B0%94%E5%BE%B7">[英]
+                                            汤姆·高尔德</a>
+                                    </span><br>
+                                    <span class="pl">出版社:</span> 后浪丨湖南美术出版社<br>
+                                    <span class="pl">原作名:</span> Goliath<br>
+                                    <span>
+                                        <span class="pl"> 译者</span>:
+                                        <a class="" href="/search/%E6%9D%A5%E5%85%B9">来兹</a>
+                                    </span><br>
+                                    <span class="pl">出版年:</span> 2020-12<br>
+                                    <span class="pl">页数:</span> 100<br>
+                                    <span class="pl">定价:</span> 45.00<br>
+                                    <span class="pl">装帧:</span> 平装<br>
+                                    <span class="pl">丛书:</span>&nbsp;<a
+                                        href="https://book.douban.com/series/31179">后浪漫</a><br>
+                                    <span class="pl">ISBN:</span> 9787541157776<br>
+                                </div>
+                            </div>
+                            <div id="interest_sectl" class="">
+                                <div class="rating_wrap clearbox" rel="v:rating">
+                                    <div class="rating_logo">豆瓣评分</div>
+                                    <div class="rating_self clearfix" typeof="v:Rating">
+                                        <strong class="ll rating_num " property="v:average"> 8.1 </strong>
+                                        <span property="v:best" content="10.0"></span>
+                                        <div class="rating_right ">
+                                            <div class="ll bigstar40"></div>
+                                            <div class="rating_sum">
+                                                <span class="">
+                                                    <a href="comments" class="rating_people"><span
+                                                            property="v:votes">35</span>人评价</a>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <span class="stars5 starstop" title="力荐">
+                                        5星
+                                    </span>
+                                    <div class="power" style="width:48px"></div>
+                                    <span class="rating_per">37.1%</span>
+                                    <br>
+                                    <span class="stars4 starstop" title="推荐">
+                                        4星
+                                    </span>
+                                    <div class="power" style="width:64px"></div>
+                                    <span class="rating_per">48.6%</span>
+                                    <br>
+                                    <span class="stars3 starstop" title="还行">
+                                        3星
+                                    </span>
+                                    <span class="rating_per">14.3%</span>
+                                    <br>
+                                    <span class="stars2 starstop" title="较差">
+                                        2星
+                                    </span>
+                                    <span class="rating_per">0.0%</span>
+                                    <br>
+                                    <span class="stars1 starstop" title="很差">
+                                        1星
+                                    </span>
+                                    <span class="rating_per">0.0%</span>
+                                    <br>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br clear="all">
+                    <div id="collect_form_35265945"></div>
+                    <div class="related_info">
+                        <h2>
+                            <span>内容简介</span>
+                            &nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·
+                        </h2>
+                        <div class="indent" id="link-report">
+                            <span class="short">
+                                <div class="intro">
+                                    <p>※汤姆•高尔德首部长篇漫画</p>
+                                    <p>※以简明的视觉元素，探讨战争与英雄主义</p>
+                                    <p>※用黑色幽默的方式，重新演绎《圣经》典故</p>
+                                    <p>※打破刻板印象，咏唱温柔巨人的命运挽歌</p>
+                                    <p>※一部独属成年人的“睡前故事”</p>
+                                    <p>------</p>
+                                    <p>◎编辑推荐</p>
+                                    <p>♠️汤姆·高尔德首部长篇漫画作品</p>
+                                    <p>本书是英国著名插画师、漫画家汤姆·高尔德的首部长篇漫画作品。《歌利亚》延续其一贯的简洁幽默画风，以明快的画面重新演绎大卫与歌利亚这一原本严肃的宗教道德寓言，黑色幽默中掺杂几分伤感。
+                                    </p>
+                                    <p>📜改编圣经典故，颠覆常规认知</p>
+                                    <p>《圣经》中大卫与歌利亚的故事家喻户晓，在原文本中，这故事的主角是少年大卫和以色列王扫罗。按高尔德自己的话来说，原文中非利士勇士歌利亚只是“一连串描绘他身材的数字和一身铠甲兵器”。汤姆·高尔德将这个故事的叙述角度从以色列人一方转到非利士人一方，为歌利亚这个人物添加了更多个性细节，让他成为一个喜欢文书工作的文...
+                                    </p>
+                                    <p><a href="javascript:void(0)" class="j a_show_full">(展开全部)</a></p>
+                                </div>
+                            </span>
+                            <span class="all hidden">
+                                <div class="">
+                                    <div class="intro">
+                                        <p>※汤姆•高尔德首部长篇漫画</p>
+                                        <p>※以简明的视觉元素，探讨战争与英雄主义</p>
+                                        <p>※用黑色幽默的方式，重新演绎《圣经》典故</p>
+                                        <p>※打破刻板印象，咏唱温柔巨人的命运挽歌</p>
+                                        <p>※一部独属成年人的“睡前故事”</p>
+                                        <p>------</p>
+                                        <p>◎编辑推荐</p>
+                                        <p>♠️汤姆·高尔德首部长篇漫画作品</p>
+                                        <p>本书是英国著名插画师、漫画家汤姆·高尔德的首部长篇漫画作品。《歌利亚》延续其一贯的简洁幽默画风，以明快的画面重新演绎大卫与歌利亚这一原本严肃的宗教道德寓言，黑色幽默中掺杂几分伤感。
+                                        </p>
+                                        <p>📜改编圣经典故，颠覆常规认知</p>
+                                        <p>《圣经》中大卫与歌利亚的故事家喻户晓，在原文本中，这故事的主角是少年大卫和以色列王扫罗。按高尔德自己的话来说，原文中非利士勇士歌利亚只是“一连串描绘他身材的数字和一身铠甲兵器”。汤姆·高尔德将这个故事的叙述角度从以色列人一方转到非利士人一方，为歌利亚这个人物添加了更多个性细节，让他成为一个喜欢文书工作的文静的巨人。歌利亚的形象在高尔德笔下变得鲜活，整个故事的氛围也随之改变。
+                                        </p>
+                                        <p>⚔️“巨人=好斗”？挑战刻板印象</p>
+                                        <p>《圣经》源文本中，体型高大的歌利亚几乎是被默认为生性残暴、好斗，因此最终落得被斩首的下场也是罪有应得。然而在汤姆·高尔德的版本中，歌利亚成了一个讨厌巡逻、讨厌舞刀弄剑、反感别人斗兽取乐、喜欢文书工作的巨人。体型高大≠凶残好斗，在高尔德的妙笔改编下，这个原本理所当然的宗教寓言罩上了一层荒诞又悲凉的色彩。
+                                        </p>
+                                        <p>◎内容简介</p>
+                                        <p>《歌利亚》根据圣经典故改编，从歌利亚的角度出发讲述故事，颠覆了人们对巨人歌利亚“生性残暴、好斗”的印象。</p>
+                                        <p>体型高大却文静木讷的歌利亚在军中服役，他天性善良、厌恶暴力，比起体力活更喜欢做文书工作。不少人想利用他高大的体型来为自己谋利。有一天，一名上尉在国王面前提出一个馊主意：派歌利亚每天去敌军阵前挑衅……
+                                        </p>
+                                        <p>在上尉的威逼利诱之下，歌利亚只好前往，每天应付公事地去两军对峙的山谷里对峙。逐渐地，歌利亚爱上了荒野中远离尘嚣的生活，决定在这里长居。</p>
+                                        <p>终于有一天，他遭遇了前来应战的大卫……</p>
+                                        <p>◎媒体推荐</p>
+                                        <p>高尔德用简单而聪明的视觉元素探讨了战争和英雄主义这些复杂的重大话题。</p>
+                                        <p>——《纽约时报》</p>
+                                        <p>《歌利亚》对故事的改编可以说是教科书级别的……将一个基督教反败为胜的颂歌变成了对政治粉饰力量和荒诞战争现实的思考。</p>
+                                        <p>——《泰晤士报》（伦敦）</p>
+                                        <p>高尔德简洁的风格完美地捕捉到了不断蚕食着歌利亚的疲倦……在一个宗教道德寓言中注入了属于现代世界的新意义，以及一种感伤且温柔的幽默。</p>
+                                        <p>——《加拿大全国邮报》</p>
+                                        <p>一个从歌利亚角度讲述的大卫与歌利亚的传说，带有悲剧性和黑色幽默成分。高尔德的作品总有一种宁静的力量，以及一种强大的情感上的吸引力。</p>
+                                        <p>——Boing Boing</p>
+                                    </div>
+
+                                </div>
+                            </span>
+                        </div>
+                        <h2>
+                            <span>作者简介</span>
+                            &nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·
+                        </h2>
+                        <div class="indent ">
+                            <div class="">
+                                <style type="text/css" media="screen">
+                                    .intro p {
+                                        text-indent: 2em;
+                                        word-break: normal;
+                                    }
+                                </style>
+                                <div class="intro">
+                                    <p>👨‍🎨[英] 汤姆•高尔德（Tom Gauld）</p>
+                                    <p>1976 年生于苏格兰阿伯丁郡，曾在爱丁堡艺术学院和皇家艺术学院学习插画。他之前的作品包括《王国守卫者》《猎人与画家》《巨型机器人》等。他从2005
+                                        年开始为《卫报》创作卡通，每周连载更新。</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+</template>
+<script>
+
+</script>
+<style scoped>
+
+</style>
---
+++ douban-vue/src/components/top250.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="content">
+        <h1>豆瓣最受欢迎的书评</h1>
+        <div class="grid-16-8 clearfix">
+            <div class="article">
+                <ul class="top-tab clearfix">
+                    <li class=" selected"> <a href="javascript:;"> <span>最受欢迎的</span> </a> </li>
+                    <li class=""> <a href="/review/latest?app_name=book"> <span>最新书评</span> </a> </li>
+                </ul>
+                <div class="review-list chart ">
+                    <div data-cid="13071563" v-for="(item,index) in items" :key="index">
+                        <div class="main review-item" id="13071563">
+                            <a class="subject-img" href="https://book.douban.com/subject/30353862/"> <img alt="黑死病"
+                                    title="黑死病" src="https://img9.doubanio.com/view/subject/m/public/s30023016.jpg"
+                                    rel="v:image"> </a>
+                            <header class="main-hd">
+                                <a href="https://www.douban.com/people/atdelilah/" class="avator">
+                                    <img width="24" height="24" src="https://img1.doubanio.com/icon/u52080104-9.jpg">
+                                </a>
+                                <a href="https://www.douban.com/people/atdelilah/" class="name">羽落弦</a>
+                                <span class="allstar10 main-title-rating" title="很差"></span>
+                                <span content="2020-12-20" class="main-meta">2020-12-20 20:28:17</span>
+                            </header>
+                            <div class="main-bd">
+                                <h2><a href="https://book.douban.com/review/13071563/">如何精心毁掉一本历史著作</a></h2>
+                                <div id="review_13071563_short" class="review-short" data-rid="13071563">
+                                    <div class="short-content">
+                                        首先一句话总结：毁掉这本书的并不是常见的翻译问题（相反，从译注可以看出译者很用心查资料），不是纸质或排版，而是“精心”的策划与图书设计。
+                                        一开始，我是听说这本书里有大量史料图片才买的，它的宣传也都是在强调“选用了大量原始资料”“知名高校图书馆珍藏”。虽然豆瓣...
+                                        &nbsp;(<a href="javascript:;" id="toggle-13071563-copy" class="unfold"
+                                            title="展开">展开</a>)
+                                    </div>
+                                </div>
+
+                                <div class="action">
+                                    <a href="javascript:;;" class="fold hidden">收起</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="aside">
+                <div class="sidebar-info-wrapper">
+                    <div class="sidebar-copy">
+                        <p>如果你觉得一篇评论对你有帮助，请你点击“有用”。你的投票直接决定哪些评论出现在豆瓣首页和“豆瓣最受欢迎的评论”里，以及在书、电影和音乐介绍页里评论的排序。</p>
+                        <p>所有“没用”的点击都是匿名的。</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:"review",
+        data(){
+            return{
+                items : [...Array(20).keys()],
+
+            }
+        },
+        
+    }
+</script>
